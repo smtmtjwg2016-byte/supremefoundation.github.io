7e5b1b0a7bd4627df5fc569ec62b47dd70b649f4
smtmtjwg2016-byte <smtmt.jwg2016@gmail.com>
2025-11-08 12:33:32 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'vrp-offline-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './sw.js',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+  if (url.origin === location.origin) {
+    e.respondWith(
+      caches.match(e.request).then(res => res || fetch(e.request))
+    );
+  } else {
+    e.respondWith(fetch(e.request).catch(() => new Response('Offline', {status: 503})));
+  }
+});
